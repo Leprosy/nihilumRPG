d62e7b39487922c04ae82029b677a84d1de7c40e
Miguel Rojas <leprosy57@gmail.com>
2023-10-29 14:26:01 -0300

    All info of monsters and groups is in

--- src/entities/MonsterManager.ts
+++ src/entities/MonsterManager.ts
@@ -2,6 +2,8 @@ import { Dungeon } from "./Dungeon";
 import { Monster } from "./Monster";
 import { Party } from "./Party";
 
+const MAX_MONSTERS = 3;
+
 // TODO merge "entities" and "helpers" into "classes"?
 export class MonsterManager {
   monsters: Monster[];
@@ -16,12 +18,25 @@ export class MonsterManager {
     ];
   }
 
+  getKey(x: number, y: number) {
+    return `${x}x${y}`;
+  }
+
   chaseParty(party: Party, dungeon: Dungeon) {
+    const oaw =  {};
     this.monsters.forEach( (monster: Monster) => {
       const pos = monster.chaseParty(party, dungeon);
-      monster.x = pos.x;
-      monster.y = pos.y;
-      monster.set3dPosition(0, 0);
+      const count = oaw[this.getKey(pos.x, pos.y)] || 0;
+
+      if (count < MAX_MONSTERS) {
+        monster.x = pos.x;
+        monster.y = pos.y;
+        monster.set3dPosition(0, 0);
+      }
+
+      oaw[this.getKey(monster.x, monster.y)] = (oaw[this.getKey(monster.x, monster.y)] || 0) + 1;
     });
+
+    console.log("OAW total", oaw);
   }
 }
